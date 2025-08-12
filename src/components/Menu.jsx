ec1ae7819e49c58776ce0337fdc19489c5e0c2a9
Kunal verma <ikunalverma.kv@gmail.com>
2025-08-12 16:11:54 +0530

    Menubar final

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,58 @@
+import gsap from "gsap";
+import { useEffect , useState} from "react";
 
-const Menu = () => {
+const Menu = ({isVisible}) => {
+
+  useEffect(()=>{
+    if (isVisible == true) 
+    {
+    gsap.fromTo(".menu-items",{
+      rotateX: -90,
+      opacity: 0,
+      
+    },{
+      rotateX: 0,
+      opacity: 1,
+      delay: 0.2,
+      stagger: 0.03,
+      duration: 0.3,
+      transformOrigin: "70% 70%",
+      
+      
+      
+      ease: "power2.out",
+    })
+    }},[isVisible]);
+
   return (
-    <div className="  w-screen h-screen bg-black        ">  
+    <div className=" perspective-container w-screen h-screen bg-black   ">  
        
-      <div className="flex justify-end w-screen mt-[10rem] overflow-hidden">
+      <div className=" flex justify-end w-screen mt-[10rem] overflow-hidden">
         <ul className="flex flex-col font-[futura] text-[8.2vh] leading-[8vh] items-end text-white mr-8 ">
-          <li><a href="#home">SHOP</a></li> 
-          <li><a href="#home">WHOLESALE</a></li>
-          <li><a href="#home">CATERING</a></li>
-          <li><a href="#home">IMPACT</a></li>
-          <li><a href="#home">STORIES</a></li>
-          <li><a href="#home">ABOUT</a></li>
-          <li><a href="#home">CONTACT</a></li>
-          <li><a href="#home">DONATE</a></li>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >SHOP </a></li>
+          </div> 
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >WHOLESALE</a></li>
+          </div>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >CATERING</a></li>
+          </div>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >IMPACT</a></li>
+          </div>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >STORIES</a></li>
+          </div>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >ABOUT</a></li>
+          </div>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >CONTACT</a></li>
+          </div>
+          <div className="menu-items hover:text-gray-200">
+            <li><a href="#home" >DONATE</a></li>
+          </div>
         </ul>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className=" relative flex mb-4 justify-between p-[15px] z-50">
+      <nav className=" relative flex mb-4 justify-between px-[10px] z-50">
         <div>
-          <img src={showMenu ===true? "/Logo2.svg": "/Logo.svg"} alt="Logo" className="h-[5rem] z-50 text-white"  />
+          <img src={showMenu ===true ? "/Logo2.svg": "/Logo.svg"} alt="Logo" className="h-[5rem] mt-4 z-50 text-white"  />
         </div>
 
-        <ul className="  flex gap-4 font-light text-[2.5vh] items-center ">
+        <ul className=" h-[3.5rem] w-auto flex gap-[4rem] font-[hevatica] text-[2.3vh] items-center ">
           {["Home", "About", "Contact"].map((item) => (
             <li key={item}>
               <a
                 href={`#${item.toLowerCase()}`}
-                className={`relative p-[4vh] transition-colors duration-300 hover:text-gray-500 group
+                className={`relative p-[1vh] transition-colors duration-300 hover:text-gray-500 group
                   ${showMenu=== true ? "text-white " : "text-black"}
                 `}
               >
                 {item}
-                <span className="absolute b bottom-[3vh] left-[4vh] w-0 h-0.5 bg-gray-500 transition-all duration-300 ease-out group-hover:w-[calc(100%-8vh)]"></span>
+                <span className="absolute  bottom-[0vh] left-[1.3vh] w-0 h-0.5 bg-gray-500 transition-all duration-300 ease-out group-hover:w-[calc(100%-2vh)]"></span>
               </a>
             </li>
           ))}
@@ -36,11 +36,11 @@ const Navbar = () => {
 
       {/* Overlay Menu */}
       <div
-        className={` fixed top-0 left-0 w-screen h-screen bg-black z-10 flex items-center justify-center transition-all duration-300 ease-linear
+        className={` fixed top-0 left-0 w-screen h-screen bg-black z-10 flex items-center justify-center transition-all  ease-[cubic-bezier(0.22,1,0.36,1)] duration-500  
           ${showMenu ? " translate-y-0" : " -translate-y-full pointer-events-none"}
         `}
       >
-        <Menu  />
+        <Menu  isVisible={showMenu}/>
       </div>
     </div>
   );
