c18ad26ae8b94bc4e87371f2fe07439b5764e31a
Kunal verma <ikunalverma.kv@gmail.com>
2025-08-13 22:00:54 +0530

    Page 3 updated

--- src/components/HoverProduct.jsx
+++ src/components/HoverProduct.jsx
@@ -8,26 +8,26 @@ const HoverProduct = ({product}) => {
         <div className="flex group ">
             <div className=" flex w-[19rem] justify-center items-center rounded-4xl bg-[#dcc0b2] z-10 gap-8 px-[1.5rem] py-[1rem] hover:cursor-pointer  group ">
                 <div><GoDotFill /></div>
-                <div className="font-[hevantica] text-[2.3vh] tracking-wider">{product.first_text}</div>
+                <div className="font-[hevantica] text-[2vh] tracking-wider">{product.first_text}</div>
                 <div className="font-[Futura] font-extralight text-[2vh] tracking-wider ">{product.second_text}</div>
-                <IoIosArrowForward className=" z-10 text-[2vh]"/>
+                <IoIosArrowForward className=" z-10 -translate-y-[0.13vh] text-[2vh]"/>
             
             </div>
-             <div className=" flex justify-center items-center pointer-events-none opacity-0 scale-95 h-5 group-hover:pointer-events-auto group-hover:opacity-100 group-hover:scale-100
-             group-hover:h-[19rem] w-[19rem] absolute rounded-4xl bg-white
+             <div className=" bg-white  flex justify-center items-center pointer-events-none opacity-0 scale-95 h-5 group-hover:pointer-events-auto group-hover:opacity-100 group-hover:scale-100
+             group-hover:h-[19rem] w-[19rem] absolute rounded-4xl 
              transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)]">
 
                
-            <div >
+            <div className=" w-[8rem] hover:cursor-pointer">
                    <img src={product.photo1} className="" />  
                    <div className="flex justify-center " >
-                       <h5 className="text-[2.2vh] font-medium w-[8rem] flex justify-center ">{product.photo_text1}</h5>
+                       <h5 className="text-[1.8vh] mt-1.5 font-[hevatica] w-[10rem] ">{product.photo_text1}</h5>
                    </div>
             </div>
-            <div> 
+            <div className="w-[8rem] hover:cursor-pointer"> 
               <img src={product.photo2} className="" />
              <div className="flex justify-center " >
-                       <h5 className="text-[2.2vh] font-medium w-[8rem] flex justify-center  ">{product.photo_text2}</h5>
+                       <h5 className="text-[1.8vh]  text-center mt-1.5 font-[hevatica] w-[8rem] flex justify-center  ">{product.photo_text2}</h5>
                    </div>
             </div>
 
--- src/pages/page3.jsx
+++ src/pages/page3.jsx
@@ -1,25 +1,101 @@
+import { useRef , useState , useEffect } from "react"
+import gsap from "gsap";
 const Page3 = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const Hoverwindow = useRef(null);
 
+  
+
+  function CircleMove(e) {
+    // const bound = document.querySelectorAll(".child")
+    const top = Hoverwindow.current.getBoundingClientRect().top
+    const left = Hoverwindow.current.getBoundingClientRect().left
+  
+    
+    const xval = e.clientX -left
+    const yval = e.clientY -top
+    console.log (xval,yval)
+    
+    gsap.to(".circle", {
+      x: xval -100 ,
+      y: yval -120,
+      
+      duration: 0.1,
+      ease: "power2.out",
+    });
+  }
+
+  function CircleEnter(){
+    console.log("heys")
+    gsap.to(".circle", {
+      scale: 1,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  }
+  function CircleLeave(){
+    gsap.to(".circle", {
+      scale: 0,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  }
 
   return (
-    <div className=" w-screen min-h-screen  ">
+    <div ref={Hoverwindow}  className=" w-screen min-h-screen  ">
 
-      <div className="flex flex-wrap justify-between items-center ">
-        <div className="w-[35rem] h-[40rem] mx-6  hover:cursor-pointer flex flex-col items-center justify-center">
-          <img src="/Page3_1.avif" className="w-[30rem]"></img>
-          <h4>Donate 20 meals to a women shelter</h4>
+      {/* mouse move circle */}
+      <div className="circle overflow-x-hidden w-[18rem] opacity-40 z-10 pointer-events-none h-[18rem] scale-0  rounded-full bg-gray-300  flex items-center absolute"> </div>
+
+
+      <div className="flex flex-wrap justify-between items-center z-30 ">
+        
+        <div onMouseMove={(e)=>{CircleMove(e)}} 
+        onMouseEnter={()=>{CircleEnter()}}
+        onMouseLeave={()=>{CircleLeave()}}
+        
+        
+        
+        
+        className="child w-[35rem] h-[40rem] mx-6 z-30 hover:cursor-pointer flex flex-col items-center justify-center">
+          <img src="/Page3_1.avif" className="w-[30rem] "></img>
+          <h4 className="font-[hevatica] uppercase text-[2.0vh] mt-2">TWO KISSES & TWO HUGS CANDLES & SPRAY PACK</h4>
         </div>
-        <div className="w-[35rem] h-[40rem] mx-6  hover:cursor-pointer flex flex-col items-center justify-center">
+
+        <div onMouseMove={(e)=>{CircleMove(e)}}
+        onMouseEnter={()=>{CircleEnter()}}
+        onMouseLeave={()=>{CircleLeave()}}
+        
+        
+        
+        
+        className="child w-[35rem] h-[40rem] mx-6 z-30 hover:cursor-pointer flex flex-col items-center justify-center">
           <img src="/Page3_2.avif" className="w-[30rem]"></img>
-          <h4>Donate 20 meals to a women shelter</h4>
+          <h4 className="font-[hevatica] uppercase text-[2.0vh] mt-2">Donate 20 meals to a women shelter</h4>
         </div>
-        <div className="w-[35rem] h-[40rem] mx-6  hover:cursor-pointer flex flex-col items-center justify-center">
+
+        <div onMouseMove={(e)=>{CircleMove(e)}} 
+        onMouseEnter={()=>{CircleEnter()}}
+        onMouseLeave={()=>{CircleLeave()}}
+        
+        
+        
+        
+        className=" child w-[35rem] h-[40rem] mx-6 z-30 hover:cursor-pointer flex flex-col items-center justify-center">
           <img src="/Page3_3.avif" className="w-[30rem]"></img>
-          <h4>Donate 20 meals to a women shelter</h4>
+          <h4 className="font-[hevatica] uppercase text-[2.0vh] mt-2">Donate 20 meals to a women shelter</h4>
         </div>
-        <div className="w-[35rem] h-[40rem] mx-6  hover:cursor-pointer flex flex-col items-center justify-center">
+
+        <div onMouseMove={(e)=>{CircleMove(e)}} 
+        onMouseEnter={()=>{CircleEnter()}}
+        onMouseLeave={()=>{CircleLeave()}}
+        
+        
+        
+        
+        className=" child w-[35rem] h-[40rem] mx-6 z-30 hover:cursor-pointer flex flex-col items-center justify-center">
           <img src="/Page3_4.avif" className="w-[30rem]"></img>
-          <h4>Donate 20 meals to a women shelter</h4>
+          <h4 className="font-[hevatica] uppercase text-[2.0vh] mt-2">Donate 20 meals to a women shelter</h4>
         </div>
 
       </div>
